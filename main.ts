c9fe5d67019f97dbea3925588769e78bd6161971
fluxens <haftbar@gmail.com>
2020-09-17 15:47:24 +0200

    removed forever

--- main.ts
+++ main.ts
@@ -58,7 +58,6 @@ input.onButtonPressed(Button.B, function () {
         basic.pause(500)
     }
 })
-let cnt = 0
 let i = 0
 let millis_in_sec = 0
 let s_final = 0
@@ -72,36 +71,4 @@ strip.setBrightness(30)
 s_main = 240
 s_grace = 25
 s_final = 30
-millis_in_sec = 50
-basic.forever(function () {
-    basic.showNumber(cnt)
-    i = 0
-    while (i != leds) {
-        basic.pause(s_main / leds * millis_in_sec)
-        strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Green))
-        strip.show()
-        i += 1
-    }
-    i = 0
-    while (i != leds) {
-        basic.pause(s_grace / leds * millis_in_sec)
-        strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Orange))
-        strip.show()
-        i += 1
-    }
-    i = 0
-    while (i != leds) {
-        basic.pause(s_final / leds * millis_in_sec)
-        strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Red))
-        strip.show()
-        i += 1
-    }
-    for (let index = 0; index < 6; index++) {
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
-        basic.pause(500)
-        strip.clear()
-        strip.show()
-        basic.pause(500)
-    }
-    cnt += 1
-})
+millis_in_sec = 10
