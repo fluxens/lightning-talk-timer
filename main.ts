83d08405251d33a293a31165751406865a011f74
fluxens <haftbar@gmail.com>
2020-09-18 11:28:25 +0200

    simplified

--- main.ts
+++ main.ts
@@ -29,38 +29,18 @@ function blinkenlights (p_s_main: number, p_s_grace: number, p_s_final: number, 
     }
 }
 input.onButtonPressed(Button.A, function () {
-    i = 0
-    while (i != leds) {
-        basic.pause(s_main / leds * 1000)
-        strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Green))
-        strip.show()
-        i += 1
-    }
-    i = 0
-    while (i != leds) {
-        basic.pause(s_grace / leds * 1000)
-        strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Orange))
-        strip.show()
-        i += 1
-    }
-    i = 0
-    while (i != leds) {
-        basic.pause(s_final / leds * 1000)
-        strip.setPixelColor(i, neopixel.colors(NeoPixelColors.Red))
-        strip.show()
-        i += 1
-    }
-    for (let index = 0; index < 6; index++) {
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
-        basic.pause(500)
-        strip.clear()
-        strip.show()
-        basic.pause(500)
-    }
+    basic.showIcon(IconNames.Happy)
+    millis_in_sec = 1000
+    blinkenlights(s_main, s_grace, s_final, millis_in_sec, leds)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
-	
+    basic.showIcon(IconNames.Ghost)
+    millis_in_sec = 40
+    blinkenlights(s_main, s_grace, s_final, millis_in_sec, leds)
+    basic.clearScreen()
 })
+let millis_in_sec = 0
 let i = 0
 let s_final = 0
 let s_grace = 0
@@ -69,8 +49,9 @@ let strip: neopixel.Strip = null
 let leds = 0
 leds = 16
 strip = neopixel.create(DigitalPin.P0, leds, NeoPixelMode.RGB)
-strip.setBrightness(30)
+strip.setBrightness(255)
+strip.clear()
+strip.show()
 s_main = 240
 s_grace = 25
 s_final = 30
-let millis_in_sec = 10
